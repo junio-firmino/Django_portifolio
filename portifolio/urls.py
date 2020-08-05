4906ca9041d7d991b8cf1d66cf6127d6072bc334
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-08-04 22:30:31 -0300

    arquivos do projeto que será construído
    Arquivos do portfólio.

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -1,7 +1,7 @@
 """portifolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls_b')),
-    path('somos/', include('somos.urls_somos')),
+    path('blog/',include('blog.urls_blog')),
 ]
